5145c37bd24f5c27cc515cf59c653b37358175ba
bingdaoqianfeng <luckliuyuxin@163.com>
2017-08-06 09:29:57 -0400

    auto clone x264wq

--- build_x264.sh
+++ build_x264.sh
@@ -1,4 +1,21 @@
+checkfail()
+{
+    if [ ! $? -eq 0 ];then
+        echo "$1"
+        exit 1
+    fi
+}
+
 . setenv.sh
+
+if [ ! -d "x264" ]; then
+    echo "X264 source not found, cloning"
+        git clone git://git.videolan.org/x264.git
+        checkfail "x264 source: git clone failed"
+else
+    echo "x264 source found"
+fi
+
 cd ./x264
 
 SOURCE=`pwd`
@@ -9,6 +26,7 @@ echo "DEST:     $DEST"
 ./configure --cross-prefix=$CROSS_PREFIX \
 --sysroot="$SYSROOT" \
 --host=arm-linux \
+--prefix=$PREFIX \
 --enable-pic \
 --enable-static \
 --extra-cflags="-fPIE -pie" \
@@ -16,4 +34,5 @@ echo "DEST:     $DEST"
 --disable-cli
 
 make -j4
-#make DESTDIR=$DEST prefix=$prefix install
+#make DESTDIR=$DESTDIR prefix=$PREFIX install
+make  install
